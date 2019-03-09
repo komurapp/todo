767db5331823f51f7333f05595ad010921f40035
komura <h.komura2@gmail.com>
2019-03-09 17:02:54 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text} from "react-native";
 
-export default class App extends React.Component {
+const styles = {
+  viewStyles: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+}
+
+class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.viewStyles}>
+        <Text>Hello there</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
